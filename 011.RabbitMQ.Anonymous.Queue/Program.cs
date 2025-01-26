84aca9e678a134828b460d6653aec7e7c406fa60
KazemJavadi <kazemjavadi@hotmail.com>
2025-01-26 18:43:20 +0330

    RabbitMQ anonymous queue

---
+++ 011.RabbitMQ.Anonymous.Queue/Program.cs
@@ -0,0 +1,11 @@
+﻿using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+var result = await channel.QueueDeclareAsync(queue: string.Empty);
+
+Console.WriteLine($"Queue is created. Queue name is \"{result.QueueName}\"");
+Console.ReadLine();
