352026e8d272c26e59debca071766099b04eac97
KazemJavadi <kazemjavadi@hotmail.com>
2025-01-26 11:04:28 +0330

    RabbitMQ consumer ack and reject

---
+++ 003.RabbitMQ.Consumer.Ack.Reject/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+var connection = await factory.CreateConnectionAsync();
+var channel = await connection.CreateChannelAsync();
+
+var consumer = new AsyncEventingBasicConsumer(channel: channel);
+
+consumer.ReceivedAsync += async (model, eventArgs) =>
+{
+    string message = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+    Console.WriteLine($"Received: {message}");
+
+    if (message.Contains("number 9"))
+        await channel.BasicRejectAsync(eventArgs.DeliveryTag, requeue: true);
+    else
+        await channel.BasicAckAsync(eventArgs.DeliveryTag, multiple: false);
+};
+
+string queueName = "q01";
+await channel.BasicConsumeAsync(queue: queueName, autoAck: false, consumer: consumer);
+
+Console.ReadLine();
