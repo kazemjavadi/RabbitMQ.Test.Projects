bb89516c4c2b7bdbaa11fe130de95ea5e78f271a
KazemJavadi <kazemjavadi@hotmail.com>
2025-01-26 10:56:29 +0330

    Simple producer and consumer

---
+++ 002.RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Xml.Schema;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+var connection = await factory.CreateConnectionAsync();
+var channel = await connection.CreateChannelAsync();
+
+var consumer = new AsyncEventingBasicConsumer(channel: channel);
+
+consumer.ReceivedAsync += async (model, eventArgs) =>
+{
+    string message = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+    Console.WriteLine($"Received: {message}");
+};
+
+string queueName = "q01";
+await channel.BasicConsumeAsync(queue: queueName, autoAck: true, consumer: consumer);
+
+Console.ReadLine();
---
+++ 01.RabbiMQ.Producer/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = await factory.CreateConnectionAsync();
+using var channel  = await connection.CreateChannelAsync();
+
+string exchangeName = "ex01";
+await channel.ExchangeDeclareAsync(exchange: exchangeName, type: ExchangeType.Direct, durable: true, autoDelete: false);
+string queueName = "q01";
+await channel.QueueDeclareAsync(queue: queueName,durable: true, exclusive: false, autoDelete: false);
+await channel.QueueBindAsync(queue: queueName, exchange: exchangeName, routingKey: string.Empty);
+
+for(int i = 0; i < 10; i++)
+{
+    string message = $"This message number {i} from Producer ({DateTime.Now})";
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    await channel.BasicPublishAsync(exchange: exchangeName, 
+        routingKey: string.Empty,
+        body: messageBody);
+}
+
+await channel.CloseAsync();
